6a51c9b7c5bfd9ee2bdfb879d584af9191b1d748
Stelios Kourlis <stelioskourlis@gmail.com>
2025-07-30 16:49:21 +0300

    Will now always play thinking animation between searching animations

--- Avatar/Assets/Scripts/AnimatorBehaviours/ThinkingBehaviour.cs
+++ Avatar/Assets/Scripts/AnimatorBehaviours/ThinkingBehaviour.cs
@@ -5,64 +5,67 @@ public class ThinkingBehaviour : StateMachineBehaviour
 {
     private Vector3 thinkingCameraPosition = new(0.09f, 1.27f, 6.65f), thinkingCameraRotation = new(15f, 155f, 0f);
     private Vector3 searchingHighCameraPosition = new(-0.665f, 1.533f, 9.107f), searchingHighCameraRotation = new(15f, 155f, 0f);
-    private Vector3 searchingLowCameraPosition = new(-0.4f, 2.7f, 8.4f), searchingLowCameraRotation = new(40f, 155f, 0f);
+    private Vector3 searchingLowCameraPosition = new(-0.5f, 2.3f, 8.7f), searchingLowCameraRotation = new(40f, 155f, 0f);
     private readonly float duration = 0.5f;
     Tween posTween, rotTween;
 
     private Vector3 cabinetShownPosition = new(0.52f, 0.024f, 6.657f), cabinetHiddenPosition = new(0.5f, 0.024f, 8.5f);
-    private Vector3 cabinetLowShownPosition = new(0.5f, 0.024f, 6.8f);
+    private Vector3 cabinetLowShownPosition = new(0.5f, -0.545f, 6.8f);
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (stateInfo.IsName("Empty"))
-        {
-            int rand = Random.Range(1, 4);
-            animator.SetInteger("ThinkingAnimation", rand);
-        }
-
         if (stateInfo.IsName("Thinking"))
         {
             animator.transform.GetComponent<AvatarBlendKeysController>().BlendEyesLookUp();
             animator.transform.GetComponent<AvatarBlendKeysController>().BlendRightEyebrowUp();
             posTween = Camera.main.transform.DOMove(thinkingCameraPosition, duration).SetEase(Ease.InOutQuad).OnComplete(() => posTween = null);
             rotTween = Camera.main.transform.DORotate(thinkingCameraRotation, duration).SetEase(Ease.InOutQuad).OnComplete(() => rotTween = null);
-        }
-        else
-        {
-            animator.transform.GetComponent<AvatarBlendKeysController>().BlendRightEyebrowDown();
-            animator.transform.GetComponent<AvatarBlendKeysController>().BlendEyesLookDown();
+            int newAnimation = Random.Range(1, 3);
+            animator.SetInteger("ThinkingAnimation", newAnimation);
         }
 
-        if (stateInfo.IsName("Searching Files High"))
+        else if (stateInfo.IsName("Searching Files High"))
         {
             GameObject.Find("Cabinet").GetComponent<Transform>().DOMove(cabinetShownPosition, duration).SetEase(Ease.InOutQuad);
             posTween = Camera.main.transform.DOMove(searchingHighCameraPosition, duration).SetEase(Ease.InOutQuad).OnComplete(() => posTween = null);
             rotTween = Camera.main.transform.DORotate(searchingHighCameraRotation, duration).SetEase(Ease.InOutQuad).OnComplete(() => rotTween = null);
         }
-        else
-        {
-            GameObject.Find("Cabinet").GetComponent<Transform>().DOMove(cabinetHiddenPosition, duration).SetEase(Ease.InOutQuad);
-        }
 
-        if (stateInfo.IsName("Searching Files Low"))
+        else if (stateInfo.IsName("Searching Files Low"))
         {
+            animator.applyRootMotion = true;
             GameObject.Find("CabinetLow").GetComponent<Transform>().DOMove(cabinetLowShownPosition, duration).SetEase(Ease.InOutQuad);
             posTween = Camera.main.transform.DOMove(searchingLowCameraPosition, duration).SetEase(Ease.InOutQuad).OnComplete(() => posTween = null);
             rotTween = Camera.main.transform.DORotate(searchingLowCameraRotation, duration).SetEase(Ease.InOutQuad).OnComplete(() => rotTween = null);
         }
-        else
-        {
-            GameObject.Find("CabinetLow").GetComponent<Transform>().DOMove(cabinetHiddenPosition, duration).SetEase(Ease.InOutQuad);
-        }
 
-        if (stateInfo.IsName("Looking Up"))
+        else if (stateInfo.IsName("Looking Up")) //Unused state
         {
             animator.transform.GetComponent<AvatarBlendKeysController>().BlendEyesLookUp();
             animator.transform.GetComponent<AvatarBlendKeysController>().BlendBothEyebrowsUp();
             posTween = Camera.main.transform.DOMove(thinkingCameraPosition, duration).SetEase(Ease.InOutQuad).OnComplete(() => posTween = null);
             rotTween = Camera.main.transform.DORotate(thinkingCameraRotation, duration).SetEase(Ease.InOutQuad).OnComplete(() => rotTween = null);
         }
-        else
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.IsName("Thinking"))
+        {
+            animator.transform.GetComponent<AvatarBlendKeysController>().BlendRightEyebrowDown();
+            animator.transform.GetComponent<AvatarBlendKeysController>().BlendEyesLookDown();
+        }
+        else if (stateInfo.IsName("Searching Files High"))
+        {
+            GameObject.Find("Cabinet").GetComponent<Transform>().DOMove(cabinetHiddenPosition, duration).SetEase(Ease.InOutQuad);
+        }
+        else if (stateInfo.IsName("Searching Files Low"))
+        {
+            animator.applyRootMotion = false;
+            GameObject.Find("CabinetLow").GetComponent<Transform>().DOMove(cabinetHiddenPosition, duration).SetEase(Ease.InOutQuad);
+            GameObject.Find("model").GetComponent<Transform>().SetPositionAndRotation(new Vector3(0.546f, -0.71f, 5.746f), Quaternion.Euler(0, 0, 0));
+        }
+        else if (stateInfo.IsName("Looking Up")) //Unused state
         {
             animator.transform.GetComponent<AvatarBlendKeysController>().BlendEyesLookDown();
             animator.transform.GetComponent<AvatarBlendKeysController>().BlendBothEyebrowsDown();
@@ -79,10 +82,12 @@ public class ThinkingBehaviour : StateMachineBehaviour
     {
         if (posTween != null && posTween.IsActive()) posTween.Kill();
         if (rotTween != null && rotTween.IsActive()) rotTween.Kill();
+        animator.applyRootMotion = false;
         animator.transform.GetComponent<AvatarBlendKeysController>().BlendEyesLookDown();
         animator.transform.GetComponent<AvatarBlendKeysController>().BlendBothEyebrowsDown();
         animator.transform.GetComponent<AvatarBlendKeysController>().BlendRightEyebrowDown();
         GameObject.Find("Cabinet").GetComponent<Transform>().DOMove(cabinetHiddenPosition, duration).SetEase(Ease.InOutQuad);
+        GameObject.Find("model").GetComponent<Transform>().SetPositionAndRotation(new Vector3(0.546f, -0.71f, 5.746f), Quaternion.Euler(0, 0, 0));
     }
 
 
